78a5eee74cbdf2be237a0796a8558d74b0cc79d6
cristian <cristian@cristian-sf20ba>
2021-04-30 15:27:27 -0300

    Trabajo Practico 3 EDM

---
+++ src/main/java/ar/edu/unju/edm/TrabajoPractico3RochaEdmApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPractico3RochaEdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPractico3RochaEdmApplication.class, args);
+	}
+
+}
